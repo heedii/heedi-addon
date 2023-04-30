0598827cb651a9b235c6b2da18672c2129a70fef
heedii <127703762+heedii@users.noreply.github.com>
2023-04-29 17:30:25 -0700

    Watermark Made

--- src/main/java/com/example/addon/hud/Watermark.java
+++ src/main/java/com/example/addon/hud/Watermark.java
@@ -7,7 +7,7 @@ import meteordevelopment.meteorclient.systems.hud.HudRenderer;
 import meteordevelopment.meteorclient.utils.render.color.Color;
 
 public class HudExample extends HudElement {
-    public static final HudElementInfo<HudExample> INFO = new HudElementInfo<>(Addon.HUD_GROUP, "example", "HUD element example.", HudExample::new);
+    public static final HudElementInfo<HudExample> INFO = new HudElementInfo<>(Addon.HUD_GROUP, "Watermark", "Displays the Name", HudExample::new);
 
     public HudExample() {
         super(INFO);
@@ -15,8 +15,8 @@ public class HudExample extends HudElement {
 
     @Override
     public void render(HudRenderer renderer) {
-        setSize(renderer.textWidth("Example element", true), renderer.textHeight(true));
+        setSize(renderer.textWidth("HeediAddon 0.1", true), renderer.textHeight(true));
 
-        renderer.text("Example element", x, y, Color.WHITE, true);
+        renderer.text("HeediAddon 0.1", x, y, Color.WHITE, true);
     }
 }
