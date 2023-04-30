0270dc0e180def213f75077aa5bc527c8cdbfffd
heedii <127703762+heedii@users.noreply.github.com>
2023-04-29 18:35:00 -0700

    Create NewModule.java

---
+++ src/main/java/com/example/addon/modules/NewModule.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class NewModule extends Module {
+    public NewModule() {
+        super(Addon.HEEDI, "IDK YET", "IDK YET");
+    }
+}
