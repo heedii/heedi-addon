188a5996d456b4fa7be0a0135a52cc351c03bd04
heedii <127703762+heedii@users.noreply.github.com>
2023-04-29 18:17:40 -0700

    Better Sprint

--- src/main/java/com/example/addon/modules/SprintPlus.java
+++ src/main/java/com/example/addon/modules/SprintPlus.java
@@ -14,5 +14,8 @@ public class SprintPlus extends Module {
     @EventHandler
     public void onTick(TickEvent.Post event) {
        if (mc.options.forwardKey.isPressed()) mc.player.setSprinting(true);
+       if (mc.options.backKey.isPressed()) mc.player.setSprinting(true);
+       if (mc.options.leftKey.isPressed()) mc.player.setSprinting(true);
+       if (mc.options.rightKey.isPressed()) mc.player.setSprinting(true);
     }
 }
